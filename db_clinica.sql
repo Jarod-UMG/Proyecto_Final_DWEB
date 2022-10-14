234bfb5157355ad402fb2025e27659dc067654e2
unknown <jmejiac18@miumg.edu.gt>
2022-10-13 21:22:14 -0600

    Primer Commit

---
+++ db_clinica.sql
@@ -0,0 +1,446 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_clinica
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `nit` int NOT NULL AUTO_INCREMENT,
+  `nombre_cliente` varchar(45) DEFAULT NULL,
+  `apellido_cliente` varchar(45) DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  PRIMARY KEY (`nit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalles_factura`
+--
+
+DROP TABLE IF EXISTS `detalles_factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalles_factura` (
+  `id_dfactura` int NOT NULL AUTO_INCREMENT,
+  `no_factura` int DEFAULT NULL,
+  `id_medicamento` int DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `precio_u` double DEFAULT NULL,
+  PRIMARY KEY (`id_dfactura`),
+  KEY `fk_dfactura_facturas_idx` (`no_factura`),
+  KEY `fk_dfactura_medicamentos_idx` (`id_medicamento`),
+  CONSTRAINT `fk_dfactura_facturas` FOREIGN KEY (`no_factura`) REFERENCES `facturas` (`no_factura`),
+  CONSTRAINT `fk_dfactura_medicamentos` FOREIGN KEY (`id_medicamento`) REFERENCES `medicamentos` (`id_medicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalles_factura`
+--
+
+LOCK TABLES `detalles_factura` WRITE;
+/*!40000 ALTER TABLE `detalles_factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalles_factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `diagnosticos`
+--
+
+DROP TABLE IF EXISTS `diagnosticos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diagnosticos` (
+  `id_diagnostico` int NOT NULL AUTO_INCREMENT,
+  `diagnostico` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_diagnostico`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diagnosticos`
+--
+
+LOCK TABLES `diagnosticos` WRITE;
+/*!40000 ALTER TABLE `diagnosticos` DISABLE KEYS */;
+INSERT INTO `diagnosticos` VALUES (1,'X');
+/*!40000 ALTER TABLE `diagnosticos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctores`
+--
+
+DROP TABLE IF EXISTS `doctores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctores` (
+  `id_doctor` int NOT NULL AUTO_INCREMENT,
+  `nombre_doctor` varchar(45) DEFAULT NULL,
+  `id_especialidad` int DEFAULT NULL,
+  `id_sucursal` int DEFAULT NULL,
+  PRIMARY KEY (`id_doctor`),
+  KEY `fk_doctores_especialidades_idx` (`id_especialidad`),
+  KEY `fk_doctores_sucursales_idx` (`id_sucursal`),
+  CONSTRAINT `fk_doctores_especialidades` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades` (`id_especialidad`),
+  CONSTRAINT `fk_doctores_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctores`
+--
+
+LOCK TABLES `doctores` WRITE;
+/*!40000 ALTER TABLE `doctores` DISABLE KEYS */;
+INSERT INTO `doctores` VALUES (1,'Carlos',1,1);
+/*!40000 ALTER TABLE `doctores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
+  `nombre_empleado` varchar(45) DEFAULT NULL,
+  `apellido_empleado` varchar(45) DEFAULT NULL,
+  `nit` int DEFAULT NULL,
+  `id_rol` int DEFAULT NULL,
+  `id_sucursal` int DEFAULT NULL,
+  PRIMARY KEY (`id_empleado`),
+  KEY `fk_empleados_roles_idx` (`id_rol`),
+  KEY `fk_empleados_sucursales_idx` (`id_sucursal`),
+  CONSTRAINT `fk_empleados_roles` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`),
+  CONSTRAINT `fk_empleados_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'Jarod','Mejia',1234,3,1),(2,'Sofia','Hernandez',4321,2,1);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `especialidades`
+--
+
+DROP TABLE IF EXISTS `especialidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `especialidades` (
+  `id_especialidad` int NOT NULL AUTO_INCREMENT,
+  `nombre_especialidad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_especialidad`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especialidades`
+--
+
+LOCK TABLES `especialidades` WRITE;
+/*!40000 ALTER TABLE `especialidades` DISABLE KEYS */;
+INSERT INTO `especialidades` VALUES (1,'Cardiologia');
+/*!40000 ALTER TABLE `especialidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `examenes`
+--
+
+DROP TABLE IF EXISTS `examenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `examenes` (
+  `id_examen` int NOT NULL AUTO_INCREMENT,
+  `nombre_examen` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_examen`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `examenes`
+--
+
+LOCK TABLES `examenes` WRITE;
+/*!40000 ALTER TABLE `examenes` DISABLE KEYS */;
+INSERT INTO `examenes` VALUES (1,'Examen de sangre');
+/*!40000 ALTER TABLE `examenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturas`
+--
+
+DROP TABLE IF EXISTS `facturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas` (
+  `no_factura` int NOT NULL AUTO_INCREMENT,
+  `nit` int DEFAULT NULL,
+  `id_empleado` int DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `id_sucursal` int DEFAULT NULL,
+  PRIMARY KEY (`no_factura`),
+  KEY `fk_facturas_sucursales_idx` (`id_sucursal`),
+  KEY `fk_facturas_clientes_idx` (`nit`),
+  KEY `fk_facturas_empleados_idx` (`id_empleado`),
+  CONSTRAINT `fk_facturas_clientes` FOREIGN KEY (`nit`) REFERENCES `clientes` (`nit`),
+  CONSTRAINT `fk_facturas_empleados` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
+  CONSTRAINT `fk_facturas_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facturas`
+--
+
+LOCK TABLES `facturas` WRITE;
+/*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `habitaciones`
+--
+
+DROP TABLE IF EXISTS `habitaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `habitaciones` (
+  `id_habitacion` int NOT NULL AUTO_INCREMENT,
+  `nombre_habitacion` varchar(45) DEFAULT NULL,
+  `id_sucursal` int DEFAULT NULL,
+  PRIMARY KEY (`id_habitacion`),
+  KEY `fk_habitaciones_sucursales_idx` (`id_sucursal`),
+  CONSTRAINT `fk_habitaciones_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `habitaciones`
+--
+
+LOCK TABLES `habitaciones` WRITE;
+/*!40000 ALTER TABLE `habitaciones` DISABLE KEYS */;
+INSERT INTO `habitaciones` VALUES (1,'A1',1),(2,'A2',1),(3,'B3',1);
+/*!40000 ALTER TABLE `habitaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historiales`
+--
+
+DROP TABLE IF EXISTS `historiales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historiales` (
+  `id_historial` int NOT NULL AUTO_INCREMENT,
+  `fecha_registro` date DEFAULT NULL,
+  `id_paciente` int DEFAULT NULL,
+  `id_sucursal` int DEFAULT NULL,
+  `id_diagnostico` int DEFAULT NULL,
+  `id_examen` int DEFAULT NULL,
+  `id_doctor` int DEFAULT NULL,
+  PRIMARY KEY (`id_historial`),
+  KEY `fk_historiales_pacientes_idx` (`id_paciente`),
+  KEY `fk_historiales_sucursales_idx` (`id_sucursal`),
+  KEY `fk_historiales_diagnosticos_idx` (`id_diagnostico`),
+  KEY `fk_historiales_examenes_idx` (`id_examen`),
+  KEY `fk_historiales_doctores_idx` (`id_doctor`),
+  CONSTRAINT `fk_historiales_diagnosticos` FOREIGN KEY (`id_diagnostico`) REFERENCES `diagnosticos` (`id_diagnostico`),
+  CONSTRAINT `fk_historiales_doctores` FOREIGN KEY (`id_doctor`) REFERENCES `doctores` (`id_doctor`),
+  CONSTRAINT `fk_historiales_examenes` FOREIGN KEY (`id_examen`) REFERENCES `examenes` (`id_examen`),
+  CONSTRAINT `fk_historiales_pacientes` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_paciente`),
+  CONSTRAINT `fk_historiales_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historiales`
+--
+
+LOCK TABLES `historiales` WRITE;
+/*!40000 ALTER TABLE `historiales` DISABLE KEYS */;
+INSERT INTO `historiales` VALUES (1,'2022-10-09',1,1,1,1,1),(2,'2022-10-09',1,1,1,1,1);
+/*!40000 ALTER TABLE `historiales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medicamentos`
+--
+
+DROP TABLE IF EXISTS `medicamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medicamentos` (
+  `id_medicamento` int NOT NULL AUTO_INCREMENT,
+  `nombre_medicamento` varchar(45) DEFAULT NULL,
+  `precio_u` double DEFAULT NULL,
+  `cantidad_stock` int DEFAULT NULL,
+  `cantidad_vendida` int DEFAULT NULL,
+  PRIMARY KEY (`id_medicamento`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicamentos`
+--
+
+LOCK TABLES `medicamentos` WRITE;
+/*!40000 ALTER TABLE `medicamentos` DISABLE KEYS */;
+INSERT INTO `medicamentos` VALUES (1,'Paracetamol',10,50,0),(2,'Tabcin Gel',7.25,10,0);
+/*!40000 ALTER TABLE `medicamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pac_x_hab`
+--
+
+DROP TABLE IF EXISTS `pac_x_hab`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pac_x_hab` (
+  `id_pac_x_hab` int NOT NULL AUTO_INCREMENT,
+  `id_paciente` int DEFAULT NULL,
+  `id_habitacion` int DEFAULT NULL,
+  PRIMARY KEY (`id_pac_x_hab`),
+  KEY `fk_pxh_habitaciones_idx` (`id_habitacion`),
+  KEY `fk_pxh_pacientes_idx` (`id_paciente`),
+  CONSTRAINT `fk_pxh_habitaciones` FOREIGN KEY (`id_habitacion`) REFERENCES `habitaciones` (`id_habitacion`),
+  CONSTRAINT `fk_pxh_pacientes` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_paciente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pac_x_hab`
+--
+
+LOCK TABLES `pac_x_hab` WRITE;
+/*!40000 ALTER TABLE `pac_x_hab` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pac_x_hab` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pacientes`
+--
+
+DROP TABLE IF EXISTS `pacientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pacientes` (
+  `id_paciente` int NOT NULL AUTO_INCREMENT,
+  `nombre_paciente` varchar(45) DEFAULT NULL,
+  `apellido_paciente` varchar(45) DEFAULT NULL,
+  `direccion_paciente` varchar(45) DEFAULT NULL,
+  `telefono_paciente` int DEFAULT NULL,
+  PRIMARY KEY (`id_paciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pacientes`
+--
+
+LOCK TABLES `pacientes` WRITE;
+/*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
+INSERT INTO `pacientes` VALUES (1,'Wilmer','Martinez','Chimaltenango X',12345678);
+/*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Enfermera/o'),(2,'Secretaria'),(3,'Jefe informatica');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sucursales`
+--
+
+DROP TABLE IF EXISTS `sucursales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sucursales` (
+  `id_sucursal` int NOT NULL AUTO_INCREMENT,
+  `nombre_sucursal` varchar(45) DEFAULT NULL,
+  `direccion_sucursal` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_sucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sucursales`
+--
+
+LOCK TABLES `sucursales` WRITE;
+/*!40000 ALTER TABLE `sucursales` DISABLE KEYS */;
+INSERT INTO `sucursales` VALUES (1,'Sucursal 1','La Antigua');
+/*!40000 ALTER TABLE `sucursales` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-13 21:18:44
